aed7a6b21f02efc1e66015ac835dbef543cfd56b
PINnew <pinnew@ya.ru>
2025-01-11 08:19:55 +0400

    add roza

--- flower_shop/orders/models.py
+++ flower_shop/orders/models.py
@@ -10,6 +10,7 @@ class Flower(models.Model):
     name = models.CharField(max_length=50)
     color = models.CharField(max_length=20)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='flowers/')  # Поле для фото
 
     def __str__(self):
         return f"{self.name} ({self.color}) - {self.price}₽"
